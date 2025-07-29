c747524a4772bccdc574499bca106bd13f5445ca
totaltaxamount <shieldscoen@gmail.com>
2025-07-28 20:40:15 -0400

    misc: disable gzip to enable brotli in nginx

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: 'standalone', // Outputs a Single-Page Application (SPA).
   // distDir: './build', // Changes the build output directory to `./dist/`.
-
+  compress: false,
    eslint: {
     ignoreDuringBuilds: true, // TODO: Jank
   },
