f139636e6e13067ef768aafe51ef1ef233ebd348
zanealoia <102438595+zanealoia@users.noreply.github.com>
2025-08-03 22:54:32 -0600

    Added 501c3 info to footer and sponsors page

--- src/app/sponsors/page.tsx
+++ src/app/sponsors/page.tsx
@@ -4,44 +4,45 @@ import Button from '../../components/button/button'
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: "The Deadbolts | Sponsors"
+    title: "The Deadbolts | Sponsors"
 }
 
 export default function Sponsors() {
-  return (
-    <div>
-      <div className={styles.support_header}>
-        <div className={styles.support_text}>
-          <h1>Support Our Team</h1>
-          <h3>Help Us Build the Future, One Robot at a Time</h3>
-          <div style={{height: '1rem'}}></div>
-          <p>
-              We’re The Deadbolts, a brand-new high school robotics team based in Boulder, Colorado, on a mission to
-              bring STEM opportunities to local students through the FIRST Robotics Competition. Our team is made up
-              of high-schoolers who are learning to design, build, and program a robot in just six weeks, all while
-              building the leadership, collaboration, and problem-solving skills that will carry us into the future.
-              <br/><br/>
-              As a rookie team, we’re starting from the ground up with no tools, no parts, and no past robots. We’re
-              aiming to raise $30,000 to fund our entire season, which includes:
-          </p>
-          <ul>
-              <li>Robot parts and build materials</li>
-              <li>Tools, electronics, and safety equipment</li>
-              <li>Competition registration and travel expenses</li>
-              <li>Workshops, outreach events, and STEM education efforts</li>
-          </ul>
-          <p>
-              We believe this program should be accessible to every student, nregardless of their financial background.
-              That’s why we’re reaching out to local businesses and community members for support. In return, our
-              sponsors receive logo placement on our robot, shirts, website, and more. Most importantly, any donation
-              is an opportunity to support the next generation of engineers, creators, and changemakers.
-          </p>
-          <br/>
-          <Button width={400} responsive={true} responsivePadding={30} size='lg' variant='outline' color='#000' href="https://forms.gle/uuZscsTApGWR8Bm5A">
-              Become a Sponsor
-          </Button>
+    return (
+        <div>
+            <div className={styles.support_header}>
+                <div className={styles.support_text}>
+                    <h1>Support Our Team</h1>
+                    <h3>Help Us Build the Future, One Robot at a Time</h3>
+                    <div style={{height: '1rem'}}></div>
+                    <p>
+                        We’re The Deadbolts, a brand-new high school robotics team based in Boulder, Colorado, on a mission to
+                        bring STEM opportunities to local students through the FIRST Robotics Competition. Our team is made up
+                        of high-schoolers who are learning to design, build, and program a robot in just six weeks, all while
+                        building the leadership, collaboration, and problem-solving skills that will carry us into the future.
+                        <br/><br/>
+                        As a rookie team, we’re starting from the ground up with no tools, no parts, and no past robots. We’re
+                        aiming to raise $30,000 to fund our entire season, which includes:
+                    </p>
+                    <ul>
+                        <li>Robot parts and build materials</li>
+                        <li>Tools, electronics, and safety equipment</li>
+                        <li>Competition registration and travel expenses</li>
+                        <li>Workshops, outreach events, and STEM education efforts</li>
+                    </ul>
+                    <p>
+                        We believe this program should be accessible to every student, nregardless of their financial background.
+                        That’s why we’re reaching out to local businesses and community members for support. In return, our
+                        sponsors receive logo placement on our robot, shirts, website, and more. Most importantly, any donation
+                        is an opportunity to support the next generation of engineers, creators, and changemakers.
+                        As a 501(c)(3) nonprofit, all donations to our team are tax-deductible to the extent permitted by law.
+                    </p>
+                    <br/>
+                    <Button width={400} responsive={true} responsivePadding={30} size='lg' variant='outline' color='#000' href="https://forms.gle/uuZscsTApGWR8Bm5A">
+                        Become a Sponsor
+                    </Button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -6,53 +6,57 @@ import Image from "next/image";
 const Footer = () => {
   // Is it a good idea to use the right image dimensions? Yes. Am I going to do it? No.
   return (
-    <div className={styles.footer_container}>
-      <div className={styles.social_media_wrapper}>
-        <Link
-          className={styles.social_icon_link}
-          href="https://github.com/teamdeadbolts"
-          target="_blank"
-          aria-label="Github"
-        >
-          <Image src="/images/logos/githubcoloricon.webp" width={100} height={100} alt="Github" /> 
-        </Link>
-        <Link
-            className={styles.social_icon_link}
-            href="https://www.instagram.com/deadbolts10980/"
-            target="_blank"
-            aria-label="Instagram"
-        >
-          <Image src="/images/logos/instagramcoloricon.webp" alt="Instagram" width={100} height={100} />
-        </Link>
-        <Link
-            className={styles.social_icon_link}
-            href="https://youtube.com/@deadbolts10980?si=1HgPkahZiKMXkQQC"
-            target="_blank"
-            aria-label="Youtube"
-        >
-          <Image src="/images/logos/youtubecoloricon.webp" alt="youtube" width={100} height={100}/>
-        </Link>
-        <Link
-            className={styles.social_icon_link}
-            href="https://www.thebluealliance.com/team/10980"
-            target="_blank"
-            aria-label="TBA"
-        >
-          <Image src="/images/logos/tbacoloricon.webp" alt="TBA" width={100} height={100}/>
-        </Link>
-      </div>
-      <div className={styles.contact_us_wrapper}>
-        <div className={styles.contact_us}>
-          <h4>Contact Us</h4>
-          <h6>——————</h6>
-          <p className={styles.deadboltsEmail}>
-            <Link href="mailto: teamdeadbolts@gmail.com ?subject=Deadbolts Robotics Inquiry">
-              teamdeadbolts@gmail.com
-            </Link>
-          </p>
+      <div className={styles.footer_container}>
+        <div className={styles.social_media_wrapper}>
+          <Link
+              className={styles.social_icon_link}
+              href="https://github.com/teamdeadbolts"
+              target="_blank"
+              aria-label="Github"
+          >
+            <Image src="/images/logos/githubcoloricon.webp" width={100} height={100} alt="Github" />
+          </Link>
+          <Link
+              className={styles.social_icon_link}
+              href="https://www.instagram.com/deadbolts10980/"
+              target="_blank"
+              aria-label="Instagram"
+          >
+            <Image src="/images/logos/instagramcoloricon.webp" alt="Instagram" width={100} height={100} />
+          </Link>
+          <Link
+              className={styles.social_icon_link}
+              href="https://youtube.com/@deadbolts10980?si=1HgPkahZiKMXkQQC"
+              target="_blank"
+              aria-label="Youtube"
+          >
+            <Image src="/images/logos/youtubecoloricon.webp" alt="youtube" width={100} height={100}/>
+          </Link>
+          <Link
+              className={styles.social_icon_link}
+              href="https://www.thebluealliance.com/team/10980"
+              target="_blank"
+              aria-label="TBA"
+          >
+            <Image src="/images/logos/tbacoloricon.webp" alt="TBA" width={100} height={100}/>
+          </Link>
         </div>
+        <div className={styles.contact_us_wrapper}>
+          <div className={styles.contact_us}>
+            <h4>Contact Us</h4>
+            <h6>——————</h6>
+            <p className={styles.deadboltsEmail}>
+              <Link href="mailto: teamdeadbolts@gmail.com ?subject=Deadbolts Robotics Inquiry">
+                teamdeadbolts@gmail.com
+              </Link>
+            </p>
+          </div>
+        </div>
+        <br />
+        <p className={styles.nonprofitStatement}>
+          The Deadbolts FRC Team is a 501(c)(3) nonprofit organization registered in the State of Colorado. Contributions are tax-deductible to the extent permitted by law. EIN available upon request.
+        </p>
       </div>
-    </div>
   );
 };
 
